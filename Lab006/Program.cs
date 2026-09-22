b084a645f7d05052a06664673dfb829b287b8f75
Pxng168 <pingp36533@gmail.com>
2026-09-22 14:37:40 +0700

    addLab006

---
+++ Lab006/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Student ID : 1690704539
+ * Name       : Lab006
+ * Section    : 129D
+ * No.        : 
+ * Course     : GI113 Computer Programming (GI)
+ */
+namespace Lab006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
